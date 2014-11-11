2b7b8725e80224761242f31e4bd9b09c23047649
Adam Avramov <fallenblood@gmail.com>
2014-11-11 08:43:46 +0200

    Add some documentation for `Clip` and `Track`.

--- cliplauncher/media/base.py
+++ cliplauncher/media/base.py
@@ -5,6 +5,24 @@ __all__ = ('Track', 'Clip')
 
 
 class Clip(object):
+    """ A clip represents a piece of media content.
+
+        `Clip` is the base class for all such clips, and implements some
+        common functionality which is shared among them. It can be named
+        and optionally looped, as well as react to a number of relevant
+        events, but does nothing on its own. In order to play a sound,
+        look at `SooperLooperClip`.
+
+        Subclasses of `Clip` implement concrete types of content which
+        can be played back -- such as an audio sample or a MIDI sequence.
+        Normally, playback is achieved by an external program -- such as
+        SooperLooper or xjadeo. The clip object contains the data needed
+        to control that program, such as the location of an audio sample
+        and the rate at which it shall be played back, as well as a way
+        to send such data to the program (e.g. via OSC).
+
+        Clips are normally grouped in `Track`s. """
+
     ON_ADD      = Event()
     ON_EDIT     = Event()
     ON_REMOVE   = Event()
@@ -24,7 +42,7 @@ class Clip(object):
     def launch(self, _):
         self.ON_LAUNCH(self)
 
-    def stop(self):
+    def halt(self):
         self.ON_HALT(self)
 
     def start(self):
@@ -44,21 +62,31 @@ class Clip(object):
     def get_fields(self):
         return (('name', 'Name', self.name),
                 ('loop', 'Loop', self.loop))
- 
+
 
 class Track(object):
+    """ A track is a container for clips.
+
+        `Track` is the base class for all tracks. It implements common
+        methods for handling a list of `Clip` objects, as well as a way
+        for `Track` subclasses to initialize a list of clips from short
+        representations -- e.g. a `XJadeoTrack` can populate itself with
+        `XJadeoClips` based on a list of paths to video files.
+
+        A `Track` can also contain things which are shared among all
+        its clips -- such as an OSC connection to an external player
+        program. """
+
     ON_ADD    = Event()
     ON_REMOVE = Event()
 
     clips      = []
     clip_class = None
     name       = None
-    width      = None
     
-    def __init__(self, name=None, clips=None, width=None):
+    def __init__(self, name=None, clips=None):
         self.name   = name or self.name
         self.clips  = self.init_clips(clips or self.clips)
-        self.width  = width or self.width
 
     def init_clips(self, clips):
         return [self.init_clip(c) for c in clips]
@@ -80,7 +108,3 @@ class Track(object):
     def get_fields(self):
         return (('name', 'Name', ''),
                 ('loop', 'Loop', True))
-
-
-#self.widget.add.set_label('foo')
-#self.widget.clips.insert(-1, clip.get_widget())
